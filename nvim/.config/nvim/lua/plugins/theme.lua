15583382e94a25a4d0324a31d1e50a9fe4e4a158
dpazj <joepaz200@gmail.com>
2025-12-23 12:58:44 +0000

    fix: nvim faint colours

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -8,6 +8,10 @@ return {
       require("everforest").setup({
         background = "hard",
         transparent_background_level = 2,
+        colours_override = function (pallette)
+          pallette.bg4 = "#9aa19d" -- bg4 was too faint
+          pallette.bg5 = "#667571" -- bg5 was too faint
+        end,
       })
     end,
   },
